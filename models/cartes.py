eddaa8f4fb88a7db4d92347f658202a28c6701eb
VotreNom <shawanbarua70@gmail.com>
2023-09-05 13:34:25 +0200

    user  infos

---
+++ models/cartes.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from sanic.exceptions import SanicException
+from sanic.log import logger
+from sqlalchemy import Column, Integer, String, Float, DateTime,ForeignKey
+from sqlalchemy.orm import relationship
+from models.users import Base
+
+class Carte(Base):
+    __tablename__ = "carte"
+    id = Column(Integer,primary_key=True)
+    user_id = Column(Integer,ForeignKey("users.id"))
+
+    def __rpr__(self):
+        return "{} {}".format(self.id,self.user_id)
