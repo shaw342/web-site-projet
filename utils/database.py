aac593061de94c89255ab64197875312232efad6
VotreNom <shawanbarua70@gmail.com>
2023-09-10 02:51:17 +0200

    data

--- utils/database.py
+++ utils/database.py
@@ -1,13 +1,18 @@
+import sys
+
+sys.path.append("models")
+
 from faunadb import query as q
 from faunadb.objects import Ref
 from faunadb.client import FaunaClient
-from models.product import Product,Base
+from product import Product, Base
+from users import User
 
 client = FaunaClient(secret="fnAFNAjjZsAAzcxXOiKy6hcr8WiJQ6j9-DtnTzpp",domain="db.eu.fauna.com")
-
+user = User(username='shawan',email ="1234bd@gmail.com",password="12345678SBSB")
 result = client.query(
     q.create(
         q.collection("my_collection"),
-        {"data":{"name":"Jane","age":134}}
+        {"data": {"surname": user.username, "email": user.email}}
     )
 )
